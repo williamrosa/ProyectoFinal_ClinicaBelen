d6f1c44b41f1f6c0f37354b848b390dadea1dce3
William Rosa <williamrosaherrera@gmail.com>
2022-03-09 22:22:32 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal_ClinicaBelen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoFinal_ClinicaBelen.MvcApplication" Language="C#" %>
---
+++ ProyectoFinal_ClinicaBelen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoFinal_ClinicaBelen.Startup))]
+namespace ProyectoFinal_ClinicaBelen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
